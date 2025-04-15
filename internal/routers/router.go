33d087416b978f9b79fc631825410a8c58650145
chrugez <thanhcuis99@gmail.com>
2025-04-15 23:56:00 +0700

    #1: Setup project and learn GinRouter

---
+++ internal/routers/router.go
@@ -0,0 +1,44 @@
+package routers
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func NewRouter() *gin.Engine{
+	r := gin.Default()
+  v1 := r.Group("/v1/2025")
+  {
+	v1.GET("/ping/:name", Pong)
+  	v1.PUT("/ping", Pong)
+  	v1.POST("/ping", Pong)
+  	v1.PATCH("/ping", Pong)
+  	v1.DELETE("/ping", Pong)
+  	v1.HEAD("/ping", Pong)
+  	v1.OPTIONS("/ping", Pong)
+  }
+
+  v2 := r.Group("/v2/2025")
+  {
+	v2.GET("/ping", Pong)
+  	v2.PUT("/ping", Pong)
+  	v2.POST("/ping", Pong)
+  	v2.PATCH("/ping", Pong)
+  	v2.DELETE("/ping", Pong)
+  	v2.HEAD("/ping", Pong)
+  	v2.OPTIONS("/ping", Pong)
+  }
+  return r
+}
+
+func Pong(c *gin.Context) {
+	// name := c.Param("name")
+	name := c.DefaultQuery("name", "chrugez")
+	uid := c.Query("uid")
+    c.JSON(http.StatusOK, gin.H{
+      "message": "ping...pong" + name,
+	  "uid": uid,
+	  "users": []string{"user1", "user2", "user3"},
+    })
+  }
