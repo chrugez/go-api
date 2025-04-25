c95b5ea5cdd5ec13ea231b97e197edfe2485f604
chrugez <thanhcuis99@gmail.com>
2025-04-25 23:52:45 +0700

    #12 Redis pool in service

--- global/global.go
+++ global/global.go
@@ -3,11 +3,13 @@ package global
 import (
 	"github.com/chrugez/go-api/pkg/logger"
 	"github.com/chrugez/go-api/pkg/setting"
+	"github.com/redis/go-redis/v9"
 	"gorm.io/gorm"
 )
 
 var (
 	Config setting.Config
 	Logger *logger.LoggerZap
+	Rdb *redis.Client
 	Mdb *gorm.DB
 )
